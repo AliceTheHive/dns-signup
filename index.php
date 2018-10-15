a2408aca265565bd94ebca3cba4497f37acbc69e
Bryan Hernandez <pdnbryan1998@gmail.com>
2018-10-15 09:42:56 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ foreach($hosts as $host) {
 
 	// If a line contains #, skip it.
 	// ISSUE: Even if # is after the main text, the line is still skipped.
-	if(strpos($host, "#") !== false) {
-		continue;
-	}
+
+	$input = explode("#", $host);
+	$host = $input[0];
 
 	// Split line by a tab, then put it into the $ip=>$url pair.
 	list($ip, $url) = explode("\t", $host);
