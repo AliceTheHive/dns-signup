fa0fbaaaae082e2e96a7098a50336fc3b68b62f0
Bryan Hernandez <pdnbryan1998@gmail.com>
2018-10-16 09:40:37 -0700

    Added all

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="css/main.css"/>
+	<script type="text/javascript">
+	$("#ioe").after(".ioe");
+	</script>
 </head>
 <body>
 	<form action="insert.php" method="post">
 		<div>
-			URL:
-			<div class="container">
-				<input type="text" pattern="[a-z]{,10}" name="url" id="ioe">
-				<span class="ioe">.ioe</span>
-			</div>
+			URL:<br/>
+			<input type="text" pattern="[a-z]{,10}" name="url"><span class="suffix">.ioe</span>
 			<br />
-			IP:
+			IP:<br/>
 			<input type="text" name="ip" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>" readonly>
 			<br />
 			<input type="submit" />
--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
 require_once("inc/db.php");
 
 $ip = $_POST["ip"];
-$url = $_POST["url"] . ".ioe";
+$url = $_POST["url"];
 
 $stmt = $mysqli->prepare("INSERT INTO domain (ip, url) VALUES (?,?);");
 $stmt->bind_param("ss", $ip, $url);
